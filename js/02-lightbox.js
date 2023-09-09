cfc8340064c8e54d607eb57e3739f59bd79ade78
Mariusz-72 <mmbwdap@gmail.com>
2023-09-09 23:17:57 +0200

    update final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+     <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `;
+};
+
+
+//galeria z danymi z tablicy galleryItems //
+const galleryMarkup = galleryItems
+    .map(createGalleryItem)
+    .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+//uruchomienie biblioteki SimpleLightbox//
+const lightbox = new SimpleLightbox(".gallery__link", {
+    
+    
+  captionsData: "alt",
+  captionDelay: 250,
+});
